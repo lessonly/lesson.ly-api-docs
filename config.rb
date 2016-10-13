74bfbbb55b6c74e12e344ef94a02bfbd13141c01
Ross Reinhardt <rreinhardt9@gmail.com>
2016-10-13 12:38:43 -0400

    Document archiving for courses, lessons, groups, and users

--- config.rb
+++ config.rb
@@ -19,6 +19,10 @@ helpers do
   end
 
   def put_request(endpoint)
+    "curl -X PUT -u \"DOMAIN:API_KEY\" \"https://api.lesson.ly/api/v1#{endpoint}\""
+  end
+
+  def put_request_with_params(endpoint)
     "curl -X PUT -H \"Content-Type: application/json\" -u \"DOMAIN:API_KEY\" -d 'JSON_PARAMS' \"https://api.lesson.ly/api/v1#{endpoint}\""
   end
 end
